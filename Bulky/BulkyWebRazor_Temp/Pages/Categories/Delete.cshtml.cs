a223b9104f4966ed62d4573bbf734b889fe27351
jasrzhang <zhangshurui1989@gmail.com>
2023-04-12 22:21:41 +0930

    Finished Category CRUD in Razor Page project

---
+++ Bulky/BulkyWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,33 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        
+        [BindProperty]
+        public Category Category { get; set; }  
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int? id)
+        {
+            Category = _db.Categories.SingleOrDefault(c => c.Id == id);
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Remove(Category);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted successfully";
+            return RedirectToPage("Index");
+        }
+    }
+}
