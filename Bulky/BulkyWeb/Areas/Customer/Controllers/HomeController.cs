daa1ead7476e5fb828f3ccb441c4cd649df55ec2
jasrzhang <zhangshurui1989@gmail.com>
2023-04-17 22:17:46 +0930

    Created Detials Page in Custmer Area

--- Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ Bulky/BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Bulky.Models;
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +9,23 @@ namespace BulkyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productsList = _unitOfWork.Product.GetAll(includeProperties:"Category");
+            return View(productsList);
+        }
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.Id == id, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
