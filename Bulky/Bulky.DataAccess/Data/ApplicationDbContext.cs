8651a4b630d922cd83e8662d4f55cc91c35d7d4b
jasrzhang <zhangshurui1989@gmail.com>
2023-04-26 22:36:10 +0930

    lesson 143. Completed order summary UI.

--- Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -18,6 +18,8 @@ namespace BulkyWeb.DataAccess.Data
         public DbSet<ApplicationUser> applicationUsers { get; set; }
         public DbSet<Company> Companys { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- Bulky/Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky/Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -12,15 +12,12 @@ namespace Bulky.DataAccess.Repository
     public class UnitOfWork : IUnitOfWork
     {
         public ICategoryRepository Category { get; private set; }
-
         public IProductRepository Product { get; private set; }
-
         public ICompanyRepository Company { get; private set; }
-
         public IShoppingCartRepository ShoppingCart { get; private set; }
-
         public IApplicationUserRepository ApplicationUser { get; private set; }
-
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IOrderDetailRepository OrderDetail { get; private set; }
         private ApplicationDbContext _db;
 
         public UnitOfWork(ApplicationDbContext db)
@@ -31,6 +28,8 @@ namespace Bulky.DataAccess.Repository
             Company = new CompanyRepository(_db);
             ShoppingCart = new ShoppingCartRepository(_db);
             ApplicationUser = new ApplicationUserRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+            OrderDetail = new OrderDetailRepository(_db);
         }
 
         public void Save()
